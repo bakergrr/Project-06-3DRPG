fd7e3a9e05f52ecc54bb007991bbbe4880cc3394
Graham Baker <bakergrr@iu.edu>
2025-12-10 06:13:43 -0500

    all but completed the exercise!!
    this should have everything in it except for a functioning goal and a reset system when the player dies (everything that needs to reset, save for a few pickups that boost score, already has an event. i just need to write the system that triggers it.)
    
    i'm so sorry! i know i said i'd have this in on tuesday, but i've been working all night and i have class in the morning. as soon as i can (and after i make up for lost sleep), the rest of the exercise will be put in

---
+++ Assets/Scripts/EnemyBehaviorEnum.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyBehaviorEnum 
+{
+  Walking = 0,
+  ChasingPlayer = 1,
+  ReturnToWalk = 2,
+  Eek = 3,
+  Stunned = 4,
+}
